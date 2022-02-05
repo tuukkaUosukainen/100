1ec0ef95f26e3b65ea7610e7ed6b6d5ece28eaf9
kapteeni <kapteeni@example.com>
2022-02-05 14:08:08 +0200

    add todo list and UI elements + and e2e tests

--- todo/cypress/integration/todo_spec.js
+++ todo/cypress/integration/todo_spec.js
@@ -5,8 +5,20 @@ describe('Sanity test', () => {
 });
 
 describe('Todo UI', () => {
+  beforeEach(() => cy.visit('http://localhost:1234/'));
   it('Renders correct page header', () => {
-    cy.visit('http://localhost:1234/');
     cy.contains('TODO');
+  });
+
+
+  it('Renders 2 placeholder todos correctly', () => {
+    cy.contains('Completed: 0 Incomplete: 2');
+    cy.get('ul').find('li').should('have.length', 2);
+  });
+
+  it('Deletes a todo on delete click', () => {
+    cy.get('#deleteButton_todoName').click();
+    cy.contains('Completed: 0 Incomplete: 1');
+    cy.get('ul').find('li').should('have.length', 1);
   });
 });
--- todo/index.js
+++ todo/index.js
@@ -1,15 +1,70 @@
-const root = document.getElementById('root');
+const { removeById, getSumOf } = require('./helpers');
 
-// Create new DOM elements
-const heading = document.createElement('h1');
-const dateParagraph = document.createElement('p');
-const hr = document.createElement('hr');
+let todos = [
+  { id: 1, name: 'todoName', completed: false },
+  { id: 2, name: 'todoTwoName', completed: false }
+];
 
-// Add element content
-heading.textContent = 'TODO';
-dateParagraph.textContent = new Date().toLocaleString();
+function render() {
+  // Create new DOM elements
+  const root = document.getElementById('root');
+  const heading = newElement('h1');
+  const dateParagraph = newElement('p');
+  const todoDataParagraph = newElement('p');
+  const hr = newElement('hr');
+  const ul = newElement('ul');
 
-// Render elements to page
-root.appendChild(heading);
-root.appendChild(dateParagraph);
-dateParagraph.appendChild(hr);
+  // Add element content
+  heading.textContent = 'TODO';
+  dateParagraph.textContent = new Date().toLocaleString();
+
+  const [sumOfCompleted, sumOfNotCompleted] = getSumOf(todos, 'completed');
+  todoDataParagraph.textContent = `Completed: ${sumOfCompleted} Incomplete: ${sumOfNotCompleted}`;
+
+  // Render elements to page
+  root.appendChild(heading);
+  root.appendChild(dateParagraph);
+  root.appendChild(ul);
+  dateParagraph.appendChild(todoDataParagraph);
+  dateParagraph.appendChild(hr);
+
+  // render todolist
+  for (let i = 0; i < todos.length; i++) {
+    const li = newElement('li');
+    const deleteButton = newElement('button');
+    deleteButton.id = 'deleteButton_' + todos[i].name;
+    deleteButton.textContent = 'Delete';
+
+    // on delete click
+    deleteButton.addEventListener('click', () => {
+      // update todos
+      const newTodos = removeById(todos, todos[i].id);
+      todos = newTodos;
+
+      // clear dom and re-render
+      clearDOM(heading, dateParagraph, todoDataParagraph, ul);
+      render();
+    });
+
+    // render todolist item
+    ul.appendChild(li);
+    li.id = todos[i].id + todos[i].name;
+    li.textContent = 'Name: ' + todos[i].name + ' completed: ' + todos[i].completed;
+    li.append(deleteButton);
+  }
+};
+
+render();
+
+function clearDOM(heading, dateParagraph, todoDataParagraph, ul) {
+  heading.remove();
+  dateParagraph.remove();
+  todoDataParagraph.remove();
+  ul.remove();
+}
+
+function newElement(type) {
+  return document.createElement(type);
+}
+
+
