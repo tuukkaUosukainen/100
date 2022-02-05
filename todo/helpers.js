8cea6c0d551a58eb1f59f3625f7ad1258691f8da
kapteeni <kapteeni@example.com>
2022-02-05 10:03:30 +0200

    add helper for adding new todo and helper test

--- todo/helpers.js
+++ todo/helpers.js
@@ -1,7 +1,12 @@
+function addNewTodo(arr, newTodoObj) {
+  return [...arr, newTodoObj];
+}
+
 function removeById(arr, removeId) {
   return arr.filter(obj => obj.id !== removeId);
 }
 
 module.exports = {
+  addNewTodo,
   removeById
 }
--- todo/tests/helpers.test.js
+++ todo/tests/helpers.test.js
@@ -1,11 +1,19 @@
-const { removeById } = require('../helpers');
+const { removeById, addNewTodo } = require('../helpers');
+
+describe('addNewTodo', () => {
+  it('Adds a new todo to todo list', () => {
+    const initialArray = [];
+    const finalArray = [{ id: 1 }];
+    const result = addNewTodo(initialArray, { id: 1 });
+    expect(result).toEqual(finalArray);
+  });
+});
 
 describe('removeById', () => {
   it('Removes element form an array', () => {
     const initialArray = [{ id: 1 }, { id: 2 }];
     const finalArray = [{ id: 1 }];
     const result = removeById(initialArray, 2);
-
     expect(result).toEqual(finalArray);
   });
 });
